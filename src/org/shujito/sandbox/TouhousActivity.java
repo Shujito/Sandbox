b9514a0b31aad71c0f03eb1fe4a30f1100f8ae22
Alberto Ramos <alberto@informaticaelectoral.com>
2015-03-27 17:34:32 -0700

    ?

--- src/org/shujito/sandbox/TouhousActivity.java
+++ src/org/shujito/sandbox/TouhousActivity.java
@@ -1,8 +1,22 @@
 package org.shujito.sandbox;
 
 import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class TouhousActivity extends ListActivity
 {
     public static final String TAG = TouhousActivity.class.getSimpleName();
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+    }
 }
